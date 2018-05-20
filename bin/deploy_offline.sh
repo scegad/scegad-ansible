dca9f9f4194601ad371b703763731edff8b048ce
Adrian Schmitz <adrian@scegad.io>
2018-05-19 20:45:58 -0400

    More offline deploy script updates

--- bin/deploy_offline.sh
+++ bin/deploy_offline.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 # Execute playbook against local host
-/opt/ansible/offline/venv/bin/ansible-playbook -i production -l $HOSTNAME -c local site.yml
+cd /opt/ansible/offline/ansible && \
+/opt/ansible/offline/venv/bin/ansible-playbook \
+  -i production \
+  -l $HOSTNAME \
+  -c local \
+  site.yml
 
